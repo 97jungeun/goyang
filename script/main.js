d7f9329af0336819e488bddaec666225dee0ca36
nadk <nadk@aladdinsys.co.kr>
2023-10-19 10:39:59 +0900

    CSS 조금 수정

--- script/main.js
+++ script/main.js
@@ -1,3 +1,18 @@
+(() => {
+    const pBig = document.querySelectorAll('P.big');
+    pBig.forEach(elem => elem.addEventListener('click', (event) => {
+        const currentTarget = event.currentTarget;
+        if(currentTarget.classList.contains('active')) {
+            currentTarget.classList.remove('active');
+
+            return;
+        }
+
+        pBig.forEach(elem => elem.classList.remove('active'));
+        currentTarget.classList.add('active');
+    }));
+})();
+
 const slides = document.querySelectorAll('.imgslide-list li');
 let currentSlide = 0;
 const slideWidth = slides[0].clientWidth;
@@ -19,18 +34,3 @@ showSlide(currentSlide);
 
 // 3초마다 슬라이드 전환
 setInterval(nextSlide, 3000);
-
-(() => {
-    const pBig = document.querySelectorAll('P.big');
-    pBig.forEach(elem => elem.addEventListener('click', (event) => {
-        const currentTarget = event.currentTarget;
-        if(currentTarget.classList.contains('active')) {
-            currentTarget.classList.remove('active');
-
-            return;
-        }
-
-        pBig.forEach(elem => elem.classList.remove('active'));
-        currentTarget.classList.add('active');
-    }));
-})();
